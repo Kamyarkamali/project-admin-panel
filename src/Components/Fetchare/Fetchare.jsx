2bbfd87d2907752e4fa3d18a59142e75cfc8ff53
kamyar kamali kamazani <kamyarkamali021@gmail.com>
2023-05-01 23:00:18 +0430

    Continuing to build the chart

--- src/Components/Fetchare/Fetchare.jsx
+++ src/Components/Fetchare/Fetchare.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { CircularProgressbar } from "react-circular-progressbar";
+import 'react-circular-progressbar/dist/styles.css';
+
+//Icons
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 //Stled
 import "./Fetchare.scss";
 
 const Fetchare = () => {
     return (
-        <div>
-            
+        <div className='fechare'>
+            <div className="top">
+                <h1>Total Revnue</h1>
+                <MoreVertIcon fontSize='small'/>
+            </div>
+            <div className="bottom">
+                <div className="facheredchart">
+                    <CircularProgressbar value={70} text='70 %'/>
+                </div>
+                <p className='title'> Total sales made today</p>
+                <p className='amount'>$ 420</p>
+                <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, facere laudantium architecto</p>
+            </div>
         </div>
     );
 };
